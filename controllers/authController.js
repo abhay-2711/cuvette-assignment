a8ea5a88a3849196131e9de8e5683676caf9b87f
abhay-zel <abhay.pratap@zel.finance>
2024-02-09 12:14:07 +0530

    delete todo

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,12 +17,14 @@ const signup = async (req, res, next) => {
 
 const signin = async (req, res, next) => {
     const {email, password} = req.body;
+    console.log(email, password);
     try {
         const validUser = await User.findOne({ email });
         if(!validUser) return next(errorHandler(404,"User not found"));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(401, "Invalid Credentials"));
         const token = jwt.sign({ id : validUser._id }, process.env.JWT_SECRET);
+        console.log(token);
         const { password: hashedPassword, ...rest } = validUser._doc;
         const expiryDate = new Date(Date.now() + 60*60*1000); //1 hour
         res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest);
